26e94c37b2db2cb7a8eaf299719bfadc453c38a6
LmanTW <82919705+LmanTW@users.noreply.github.com>
2024-07-10 18:53:55 +0800

    Experimenting with the framework

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
-import sys
 from os import path
+import sys
+
 sys.path.append(path.dirname(__file__))
 
-from src.game import Arkanoid
-# TODO 設定遊戲class路徑
+from src.game import Bomb
+
 GAME_SETUP = {
-    "game": Arkanoid
+    "game": Bomb
 }
+
+# python3 -m mlgame -i ./ml/ml_play_manual.py .
